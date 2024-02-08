62c494959ed0ab9a9ff92898aca4ddae559ad61f
jaimin25 <jaiminchovatia25123@gmail.com>
2024-02-08 22:05:29 +0530

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,58 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: ['standard', 'plugin:react/recommended', 'plugin:react/jsx-runtime', 'prettier'],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react', 'simple-import-sort'],
+    rules: {
+        'simple-import-sort/imports': 'error',
+        'simple-import-sort/exports': 'error',
+    },
+    settings: {
+        react: {
+            version: 'detect', // Automatically includes the React version
+        },
+    },
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+        {
+            files: ['**/*.js', '**/*.ts', '**/*.tsx'],
+            rules: {
+                'simple-import-sort/imports': [
+                    'error',
+                    {
+                        groups: [
+                            // `react` first, `next` second, then packages starting with a character
+                            ['^react$', '^next', '^[a-z]'],
+                            // Packages starting with `@`
+                            ['^@'],
+                            // Packages starting with `~`
+                            ['^~'],
+                            // Imports starting with `../`
+                            ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+                            // Imports starting with `./`
+                            ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+                            // Style imports
+                            ['^.+\\.s?css$'],
+                            // Side effect imports
+                            ['^\\u0000'],
+                        ],
+                    },
+                ],
+            },
+        },
+    ],
+};
