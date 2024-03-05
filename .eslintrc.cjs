ff95456af01f92ae6d5f645070c253bdf0843adf
jaimin25 <jaiminchovatia25123@gmail.com>
2024-03-05 12:01:30 +0530

    chore: eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -28,10 +28,6 @@ module.exports = {
       {
         trailingComma: 'es5',
         tabWidth: 2,
-        semi: true,
-        singleQuote: true,
-        endOfLine: 'lf',
-        printWidth: 65,
       },
     ],
   },
